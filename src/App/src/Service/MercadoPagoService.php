5eb1e1711c0f2ea1dfc2b811335d0aac90e49482
amomercadopago <110522549+amomercadopago@users.noreply.github.com>
2022-08-08 15:23:26 +0300

    Update MercadoPagoService.php

--- src/App/src/Service/MercadoPagoService.php
+++ src/App/src/Service/MercadoPagoService.php
@@ -63,11 +63,11 @@ class MercadoPagoService
 //             ],
             'items' => $data['products'],
             'external_reference' => 'alodia@team.amocrm.com',
-//             'back_urls' => [
+            'back_urls' => [
 //                 'success' => 'https://amomercadopago.github.io/?page=payment&result=success',
 //                 'pending' => 'https://amomercadopago.github.io/?page=payment&result=pending',
 //                 'failure' => 'https://amomercadopago.github.io/?page=payment&result=failure',
-//             ],
+            ],
             'payment_methods' => [
                 'excluded_payment_methods' => [
                     ['id' => 'visa'],
